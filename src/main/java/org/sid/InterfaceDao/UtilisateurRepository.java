5eb8a2e0783924adb48d0b3e2930ec5cb94a7646
Robin FREDON <robin.fredon@gmail.com>
2019-06-06 15:16:28 +0200

    Added User Service

--- src/main/java/org/sid/InterfaceDao/UtilisateurRepository.java
+++ src/main/java/org/sid/InterfaceDao/UtilisateurRepository.java
@@ -1,5 +1,8 @@
 package org.sid.InterfaceDao;
 
+import javax.validation.Valid;
+
+import org.sid.entities.Idee;
 import org.sid.entities.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -8,5 +11,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("*")
 @RepositoryRestResource
 public interface UtilisateurRepository  extends JpaRepository<Utilisateur, Long>{
+
 
 }
---
+++ src/main/java/org/sid/service/UserService.java
@@ -0,0 +1,45 @@
+package org.sid.service;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.sid.InterfaceDao.UtilisateurRepository;
+import org.sid.entities.Idee;
+import org.sid.entities.Utilisateur;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+public class UserService {
+	@Autowired
+	private UtilisateurRepository utilisateurRepository;
+	
+	@RequestMapping(value="/users", method= RequestMethod.POST)
+	public @Valid Utilisateur SaveUtilisateur(@Valid @RequestBody Utilisateur i ){
+		return utilisateurRepository.save(i);
+	}
+	
+	@RequestMapping(value="/users", method=RequestMethod.GET)
+	public List<Utilisateur> getUtilisateurs(){
+		return utilisateurRepository.findAll();
+	}
+	
+	@RequestMapping(value="/user/{id}", method=RequestMethod.GET)
+	public Utilisateur getUtilisateur(@PathVariable Long id){
+		return utilisateurRepository.findById(id).orElse(null);
+	}
+	
+	@RequestMapping(value="/user/{id}", method=RequestMethod.PUT)
+	public Utilisateur updateUtilisateur(@RequestBody Utilisateur i){
+		return utilisateurRepository.save(i);
+	}
+	
+	
+}
