702d14dd7c25949999d188a6da7cece88ea85d98
Robin FREDON <robin.fredon@gmail.com>
2019-06-06 17:09:09 +0200

    Modified sql scripts

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,1 @@
-INSERT INTO genre (idGenre,genre) VALUES (1,"caleçon");
-INSERT INTO idee (idIdee,dataCreation,auteur,idee,titre,idGenre,idTypeIdee)VALUES (1,1559816937,'robin','quatro','caleçon',1,1);
+INSERT INTO genre (idGenre,genre) VALUES (1,"caleçon"); INSERT INTO idee (idIdee,dataCreation,auteur,idee,titre,idUtilisateur,idGenre,idTypeIdee)VALUES (1,1559816937,'robin','quatro','caleçon',1,1,1);
