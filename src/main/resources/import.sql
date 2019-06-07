7d97c03aba2351c73be737376576fd2fc6f2a76e
Robin FREDON <robin.fredon@gmail.com>
2019-06-07 11:06:20 +0200

    Fixed import user

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,6 +2,6 @@ INSERT INTO genre VALUES (1,'dragon');
 INSERT INTO genre VALUES (2,'communiste');
 INSERT INTO genre VALUES (3,'fantaisie');
 
-INSERT INTO utilisateur VALUES (1,'robin','wood','1970-01-19T01:17:07.530+0000','M','batman','bonjour','vin@hotmail.com',20,null,null);
+INSERT INTO utilisateur VALUES (1,'robin','wood','1970-01-19T01:17:07.530+0000','M','batman','bonjour','vin@hotmail.com',20);
 
 INSERT INTO idee VALUES (1,'07/06/2019','quatro','caleçon',1,1,null);
