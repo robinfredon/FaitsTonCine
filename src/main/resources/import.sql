9226567170c34df15003fe4d0e5a5f327e9fe58f
vins <vincent.rizzetto2@gmail.com>
2019-06-07 12:14:44 +0200

    modification correcte

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,8 +10,8 @@ INSERT INTO utilisateur (id_utilisateur, prenom, nom, date_creation, sexe, pseud
 
 INSERT INTO utilisateur (id_utilisateur, prenom, nom, date_creation, sexe, pseudo, mot_de_passe, email, age) VALUES (4,'vincent','rizzetto','1970-01-19T01:17:07.530+0000','M','vins','moto','vincent@hotmail.com',25);
 
-INSERT INTO idee VALUES (1,'07/06/2019','quatro','calecon',1,null,1);
+INSERT INTO idee VALUES (1,'07/06/2019','le boxeur qui boxe tout','Rocky',1,null,1);
 
-INSERT INTO idee VALUES (2,'07/06/2019','premier','test des idee',1,null,1);
+INSERT INTO idee VALUES (2,'07/06/2019','Au lieu d'utiliser un trou de verre il se teleporte','interstelar',1,null,1);
 
 INSERT INTO personnage (id_personnage, age, biographie, nom, prenom, sexe, surnom, id_utilisateur) VALUES (1,30,'Le tireur qui tire plus vite que son ombre','lucky','luck','F','luckyne',1);
