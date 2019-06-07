ee14aba8cc33896762f984c85321f239f6570fe1
Robin FREDON <robin.fredon@gmail.com>
2019-06-07 11:18:16 +0200

    Updated sql script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,6 +2,6 @@ INSERT INTO genre VALUES (1,'dragon');
 INSERT INTO genre VALUES (2,'communiste');
 INSERT INTO genre VALUES (3,'fantaisie');
 
-INSERT INTO utilisateur VALUES (1,'robin','wood','1970-01-19T01:17:07.530+0000','M','batman','bonjour','robin@hotmail.com',20);
+INSERT INTO utilisateur (id_utilisateur, prenom, nom, date_creation, sexe, pseudo, mot_de_passe, email, age) VALUES (1,'robin','wood','1970-01-19T01:17:07.530+0000','M','batman','bonjour','robin@hotmail.com',20);
 
 INSERT INTO idee VALUES (1,'07/06/2019','quatro','caleçon',1,null,1);
