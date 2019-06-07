913b754c58aa66c1cf9358ef0ff46210d626fd9d
Robin FREDON <robin.fredon@gmail.com>
2019-06-07 09:56:49 +0200

    Updated sql script

--- src/main/resources/data-postgresql.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO genre (idGenre,genre) VALUES (1,"caleçon"); INSERT INTO idee (idIdee,dataCreation,auteur,idee,titre,idGenre,idTypeIdee)VALUES (1,1559816937,'robin','quatro','caleçon',1,1);
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO genre (idGenre,genre) VALUES (1,"caleçon"); 
-INSERT INTO idee (idIdee,dataCreation,auteur,idee,titre,utilisateur,idGenre,idTypeIdee)VALUES (1,1559816937,'robin','quatro','caleçon',1,1,1);
+INSERT INTO genre (idGenre,genre) VALUES (1,'caleçon'); 
+INSERT INTO idee (idIdee,dateCreation,auteur,idee,titre,idUtilisateur,idGenre,idTypeIdee)VALUES (1,1559816937,'robin','quatro','caleçon',1,1,1);
