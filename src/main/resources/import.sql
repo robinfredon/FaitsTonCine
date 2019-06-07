49ea36bc1cf39b5fc649f38fa8e6d1559683753f
vins <vincent.rizzetto2@gmail.com>
2019-06-07 12:00:31 +0200

    modif import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,6 +2,16 @@ INSERT INTO genre VALUES (1,'dragon');
 INSERT INTO genre VALUES (2,'communiste');
 INSERT INTO genre VALUES (3,'fantaisie');
 
-INSERT INTO utilisateur (id_utilisateur, prenom, nom, date_creation, sexe, pseudo, mot_de_passe, email, age) VALUES (1,'robin','wood','1970-01-19T01:17:07.530+0000','M','batman','bonjour','robin@hotmail.com',20);
+INSERT INTO utilisateur (id_utilisateur, prenom, nom, date_creation, sexe, pseudo, mot_de_passe, email, age) VALUES (1,'louis','nardot','1970-01-19T01:17:07.530+0000','M','Louis','louis1234','louis@gmail.com',20);
 
-INSERT INTO idee VALUES (1,'07/06/2019','quatro','caleÃ§on',1,null,1);
+INSERT INTO utilisateur (id_utilisateur, prenom, nom, date_creation, sexe, pseudo, mot_de_passe, email, age) VALUES (2,'robin','fredon','1970-01-19T01:17:07.530+0000','M','robin','bonjour','robin@hotmail.com',21);
+
+INSERT INTO utilisateur (id_utilisateur, prenom, nom, date_creation, sexe, pseudo, mot_de_passe, email, age) VALUES (3,'nicolas','ferreira','1970-01-19T01:17:07.530+0000','M','nico','nuit','nico@hotmail.com',25);
+
+INSERT INTO utilisateur (id_utilisateur, prenom, nom, date_creation, sexe, pseudo, mot_de_passe, email, age) VALUES (4,'vincent','rizzetto','1970-01-19T01:17:07.530+0000','M','vins','moto','vincent@hotmail.com',25);
+
+INSERT INTO idee VALUES (1,'07/06/2019','quatro','calecon',1,null,1);
+
+INSERT INTO idee VALUES (2,'07/06/2019','premier','test des idee',1,null,1);
+
+INSERT INTO personnage VALUES (1,'lucky','luck','luckyné',30,'F','Le tireur qui tire plus vite que son ombre',1);
